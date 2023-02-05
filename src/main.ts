114c0982fe31d1f4e10d787112c93a3e273368d1
arkadii.zelenskyi <arkady.zelensky@yalantis.net>
2023-02-05 21:31:07 +0200

    added pipeline for docker deployment

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT || 8080;
   const app = await NestFactory.create(AppModule);
-  console.log('Starting app on port ' + process.env.PORT);
-  await app.listen(process.env.PORT);
+  console.log('Starting app on port ' + port);
+  await app.listen(port);
 }
 bootstrap();
