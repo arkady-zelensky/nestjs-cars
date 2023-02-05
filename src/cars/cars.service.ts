f13ae3c2a7ea1e0c58a97f0e1244b210c14c2681
arkadii.zelenskyi <arkady.zelensky@yalantis.net>
2023-02-05 14:28:38 +0200

    added logs

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -8,15 +8,18 @@ const memory = {
 export class CarsService {
   async getAll() {
     const cars: any[] = memory.cars;
+    console.log('getAll');
     // const cars: Array<any> = memory.cars;
     return cars;
   }
 
   async getById(id: number) {
+    console.log('getById');
     return memory.cars.find((car) => car.id === id);
   }
 
   async createCar(dto: any): Promise<any> {
+    console.log('createCar');
     if (memory.cars.length > 0) {
       dto.id = memory.cars[memory.cars.length - 1].id + 1;
     } else {
@@ -27,11 +30,13 @@ export class CarsService {
   }
 
   async updateCar(id: number, dto: any): Promise<void> {
+    console.log('updateCar');
     const ind = memory.cars.findIndex((car) => car.id === id);
     memory.cars[ind] = dto;
   }
 
   async deleteById(id: number) {
+    console.log('deleteById');
     memory.cars = memory.cars.filter((car) => car.id !== id);
   }
 }
